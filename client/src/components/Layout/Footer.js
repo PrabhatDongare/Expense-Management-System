564635e61fe8e5b290fb2c2fccae585c1d247564
Prabhat Dongare <dongareprabhat241@gmail.com>
2023-07-07 19:34:15 +0530

    initial commit

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <div className=" bg-dark text-light p-3">
+      <h6 className="text-center text-white">
+        All rights reserved &copy; Prabhat Dongare
+
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ client/src/config.js
@@ -0,0 +1,4 @@
+export const getBackendUrl = () => {
+    return "http://localhost:8080";
+  };
+  
